cdcb62296612eeb244bea46d891f6c02268baa40
OctavianLee <octavianlee1@gmail.com>
2015-01-23 13:55:00 +0800

    finish the shell sort

--- sorts/insertion_sort.py
+++ sorts/insertion_sort.py
@@ -18,3 +18,19 @@ class InsertionSort(SortTemplate):
                 self.items[i + 1] = self.items[i]
                 i -= 1
             self.items[i + 1] = index
+
+    def shell_sort(self):
+        """uses the shell sort algorithm to sort.
+        This is a shell sort algorithm
+        """
+
+        gap = self.length // 3 + 1
+        while gap >= 1:
+            for j in range(gap, self.length):
+                i = j - gap
+                index = self.items[j]
+                while index < self.items[i] and i>=0:
+                    self.items[i + gap] = self.items[i]
+                    i = i - gap
+                self.items[i + gap] = index
+            gap = gap // 3
