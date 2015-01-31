caa07dba3036625ceca18204ac98109aa093e207
OctavianLee <octavianlee1@gmail.com>
2015-01-31 23:28:18 +0800

    adds the linked list and the tests for it

---
+++ structures/lists.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+class Node(object):
+    """Creates a class of node.
+    For different lists, 
+    a node is a basic element to record data and the next node.
+
+    Attributes:
+        datum: a datum of a node.
+        next: the next of a node.
+    """
+    def __init__(self, datum, next):
+        """Initializes the class.
+        
+        Args:
+            datum: the datum of a node.
+            next: the next of a node.
+        """
+        self.__datum = datum
+        self.__next = next
+
+    @property
+    def datum(self):
+        """Gets a datum.
+        """
+        return self.__datum
+        
+    @property
+    def next(self):
+        """Gets the next.
+        """
+        return self.__next
+
+    @next.setter
+    def next(self, next):
+        """Sets the next node
+        """
+        self.__next = next
+
+
+class LinkedList(object):
+    """Creates a class of the linked list.
+    This a simplest linked list becaues of implementing some basic methods.
+    
+    Such as:
+        Appends and removes a node.
+        Purges and Tests if it is empty.
+
+    Attributes:
+        head: the head of a linked list.
+        tail: the tail of a linked list.
+    """
+
+    def __init__(self):
+        """Initializes the class.
+        """
+        self.__head = None
+        self.__tail = None
+
+    @property
+    def head(self):
+        """Gets the head.
+        """
+        return self.__head
+
+    @property
+    def tail(self):
+        """Gets the tail.
+        """
+        return self.__tail
+
+    def purge(self):
+        """Cleans the linked list
+        """
+        self.__head = None
+        self.__tail = None
+
+    def is_empty(self):
+        """Test whether the linked list is empty.
+        """
+        return self.__head is None
+
+    def append(self, item):
+        """Adds a node.
+        
+        Args:
+            item : the item it will add.
+        """
+        #Generates the node.
+        datum = Node(item, None)
+        #For the first node in a linked list, it will change the head.
+        if self.__head is None:
+            self.__head = datum
+        else:
+            self.__tail.next = datum
+        self.__tail = datum
+
+    def remove(self, item):
+        """removes a node.
+        
+        Args:
+            item : the item it will remove.
+        """
+        pointer = self.__head
+        prepointer = None
+        #Finds the item
+        while pointer and pointer.datum != item:
+            prepointer = pointer
+            pointer = pointer.next
+        #Raise the error when not finding the item.
+        if pointer is None: raise KeyError
+        #When the linked list has one node, it will sets the head.
+        if pointer == self.__head:
+            self.__head = pointer.next
+        else:
+            prepointer.next = pointer.next
+        if pointer == self.__tail:
+            self.__tail = prepointer
+
--- tests/test_structures.py
+++ tests/test_structures.py
@@ -4,6 +4,8 @@ import unittest
 from copy import copy
 
 from structures.arrays import *
+from structures.lists import *
+
 
 class StructureTestCase(unittest.TestCase):
     """Creates a basic testcase to test different data structures.
@@ -71,3 +73,82 @@ class MultiDimensionalArrayTest(StructureTestCase):
                     self.array[i, j, k] = (i, j, k)
                     self.assertEqual(self.array[i, j, k], (i, j, k),
                                      "The data are not matched")
+                    
+class NodeTest(StructureTestCase):
+    """Tests the class of Node.
+    """
+
+    def setUp(self):
+        """Sets up the Node.
+        """
+        self.node1 = Node(1, None)
+        self.node0 = Node(0, self.node1)
+
+    def test_datum(self):
+        """Tests to get the datum of a node.
+        """
+        self.assertEqual(self.node0.datum, 0,
+                         "Cannot get the datum in node2")
+        self.assertEqual(self.node1.datum, 1,
+                         "Cannot get the datum in node1")
+
+    def test_next(self):
+        """Tests to get the next of a node.
+        """
+        self.assertEqual(self.node0.next, self.node1,
+                         "cannot get the next in node2")
+        self.node0.next = None
+        self.assertEqual(self.node0.next, None,
+                         "cannot set the next in node2")
+        self.assertEqual(self.node1.next, None,
+                         "Cannot get the next in node1")
+
+
+class LinkedListTest(StructureTestCase):
+    """Tests the class of LinkedList.
+    """
+
+    def setUp(self):
+        """Sets up the list.
+        """
+        self.linkedlist = LinkedList()
+        self.linkedlist.append(1)
+
+    def test_head(self):
+        """Tests to get the head of the linked list.
+        """
+        self.assertEqual(self.linkedlist.head.datum, 1,
+                         "Cannot get the head")
+
+    def test_tail(self):
+        """Tests to get the tail of the linked list.
+        """
+        self.assertEqual(self.linkedlist.tail.datum, 1,
+                         "Cannot get the head")
+
+    def test_append(self):
+        """Tests to append a datum.
+        """
+        self.linkedlist.append(2)
+        self.assertEqual(self.linkedlist.tail.datum, 2,
+                         "Cannot append a datum")
+
+    def test_remove(self):
+        """Tests to remove a datum.
+        """
+        with self.assertRaises(KeyError):
+            self.linkedlist.remove(11)
+        self.linkedlist.remove(1)
+        self.assertEqual(self.linkedlist.tail, self.linkedlist.head,
+                         "Cannot remove a datum")
+
+    def test_purge(self):
+        """Tests to purge a linked list.
+        """
+        self.assertEqual(self.linkedlist.is_empty(), False,
+                         "Cannot get False when calling is_empty")
+        self.linkedlist.purge()
+        self.assertEqual(self.linkedlist.is_empty(), True,
+                         "Cannot get Treu when calling is_empty")
+
+
